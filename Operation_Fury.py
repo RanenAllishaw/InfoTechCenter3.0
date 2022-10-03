fee816ec71ca9574d802eb5bb0fa649bce2e2bef
Ranen Allishaw <ranens.work@gmail.com>
2022-10-03 08:07:26 -0400

    Added Print Statements to Welcome Visitors and to show loading of the OS

--- Operation_Fury.py
+++ Operation_Fury.py
@@ -6,3 +6,24 @@
 Our welcome message will start our program letting
 the drivers know that the InfoTechCenter OS is loading.
 """
+
+print('\nWelcome to Operation Fury InfoTech Center')
+
+x = 0
+a = '.'
+import time
+
+while x != 3:
+    if x == 0:
+        print("Operation Fury's Operating System is Booting Up")
+    time.sleep(1)
+    x += 1
+    if x == 1:
+        print("Operation Fury's Operating System is Booting Up" + a)
+    if x == 2:
+        print("Operation Fury's Operating System is Booting Up" + a + a)
+    if x == 3:
+        print("Operation Fury's Operating System is Booting Up" + a + a + a)
+    if x == 3:
+        time.sleep(1)
+        x -= 3
