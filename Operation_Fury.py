fb3cd6e9172751096415504a58b6542bd0b6ca94
Ranen Allishaw <ranens.work@gmail.com>
2022-10-05 08:38:55 -0400

    Initial Weather Commit

---
+++ Operation_Fury.py
@@ -0,0 +1,11 @@
+# Weather
+# Developer: Ranen Allishaw
+# Version 1.0
+
+"""
+Create a function for our current weather using the
+random.choice function to determine what the weather is
+picking from a list - using If, Elif, Else statements to
+check the condition of the road and print specific message.
+"""
+
