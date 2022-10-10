5b7d23f30e9631064f3ae4e4a7ca5f0f415dde3d
Ranen Allishaw <ranens.work@gmail.com>
2022-10-10 08:39:43 -0400

    Defined the Gas Level Gauge function

---
+++ Operation_Fury.py
@@ -0,0 +1,22 @@
+# Gasoline
+# Developer: Ranen Allishaw
+# Version 1.0
+
+"""
+Define a function to check our gas gauge and determine how far we can
+go before we need gas based on if, elif, and else conditions.
+"""
+
+# Import libraries here.
+import random
+
+
+# Gas level finder function.
+def gasLevelGauge():
+    gasLevelList = ['\033[3;31;40mEmpty', '\033[2;31;40m1/4 Tank', '\033[2;33;40m1/2 Tank', '\033[2;32;40m3/4 Tank',
+                    '\033[1;32;40mFull']
+    currentGasLevel = random.choice(gasLevelList)
+    return currentGasLevel
+
+
+
