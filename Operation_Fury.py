c8cd96f89784f9561ca6dba14e3cf55cf5b8edb8
Ranen Allishaw <ranens.work@gmail.com>
2022-10-11 08:38:44 -0400

    Made a list of Gas Stations and a random choice to choose the closest one.

--- Operation_Fury.py
+++ Operation_Fury.py
@@ -25,6 +25,16 @@ def gasLevelGauge():
 gasLevelIndicator = gasLevelGauge()
 
 
+def listOfGasStations():
+    gasStations = ['Shell', 'Meijer', 'Circle K', 'Marathon', 'Speedway', "Love's", 'Mobil']
+    gasStationNearby = random.choice(gasStations)
+    return gasStationNearby
+
+
+# Variable calling the listOfGasStations function to store the value once.
+nearestGasStation = listOfGasStations()
+
+
 def gasLevelAlert():
     x = 0
     a = 0
@@ -43,6 +53,18 @@ def gasLevelAlert():
                 a = 0
             if x == 8:
                 print('\033[1;33;40m Your Contacts have been notified.')
+    elif gasLevelIndicator == '\033[2;31;40m1/4 Tank':
+        print('\033[1;31;40m*Warning!*')
+        time.sleep(.5)
+        print("\033[2;33;40mYou're at 1/4 of gas left, now checking google maps for the closest gas station.")
+        time.sleep(.75)
+        print('The closest gas station to your area is; ' + nearestGasStation + '.')
+    elif gasLevelIndicator == '\033[2;33;40m1/2 Tank':
+        print('hi')
+    elif gasLevelIndicator == '\033[2;32;40m3/4 Tank':
+        print('hi')
+    elif gasLevelIndicator == '\033[1;32;40mFull':
+        print('hi')
 
 
 gasLevelAlert()
