12838a85147accf9b11f0fac0577b590c93851fc
Ranen Allishaw <ranens.work@gmail.com>
2022-10-11 08:22:10 -0400

    Updated Empty Warning

--- Operation_Fury.py
+++ Operation_Fury.py
@@ -1,6 +1,6 @@
 # Gasoline
 # Developer: Ranen Allishaw
-# Version 1.0
+# Version 1.2
 
 """
 Define a function to check our gas gauge and determine how far we can
@@ -9,6 +9,8 @@ go before we need gas based on if, elif, and else conditions.
 
 # Import libraries here.
 import random
+import time
+import sys
 
 
 # Gas level finder function.
@@ -24,8 +26,23 @@ gasLevelIndicator = gasLevelGauge()
 
 
 def gasLevelAlert():
+    x = 0
+    a = 0
     if gasLevelIndicator == '\033[3;31;40mEmpty':
-        print('\033[3;31;40m***WARNING YOU ARE ON EMPTY***\n*Calling Emergency Contacts*')
+        print('\033[3;31;40m***WARNING YOU ARE ON EMPTY***')
+        print('\033[3;31;40m**Contacting Emergency Contacts**')
+        time.sleep(.5)
+        while x != 8:
+            x += 1
+            b = ('\033[2;31;40m*Calling' + "." * a + '*')
+            a = a + 1
+            # \r prints a carriage return first, so `b` is printed on top of the previous line.
+            sys.stdout.write('\r' + b)
+            time.sleep(0.5)
+            if a == 4:
+                a = 0
+            if x == 8:
+                print('\033[1;33;40m Your Contacts have been notified.')
 
 
 gasLevelAlert()
