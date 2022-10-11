524df8740276c9c2624372304a6e7aa69c5675ed
Ranen Allishaw <ranens.work@gmail.com>
2022-10-11 07:52:02 -0400

    Cleaned up Program

--- Operation_Fury.py
+++ Operation_Fury.py
@@ -1,3 +1,12 @@
+# ********************************************************************************
+
+# Import any Libraries Here
+import time  # I imported the time library for further use in waiting in code.
+import sys  # I imported the system library for further use in controlling the terminal.
+import random  # I imported the random library for further use in randomizing.
+
+# ********************************************************************************
+
 # Welcome Message
 # Developer: Ranen Allishaw
 # Version: 1.1
@@ -6,10 +15,6 @@
 Our welcome message will start our program letting
 the drivers know that the InfoTechCenter OS is loading.
 """
-
-# Import any Libraries Here
-import time  # I imported the time library for further use in code.
-import sys  # I imported the system library for further use in code.
 
 print('\n\033[1;34;48m Welcome to Operation Fury InfoTech Center')
 
@@ -23,12 +28,14 @@ while x != 12:
     x += 1
     b = ("\033[1;33;40m Operation Fury OS is Loading" + "." * a)
     a = a + 1
-    sys.stdout.write('\r'+b) # \r prints a carriage return first, so `b` is printed on top of the previous line.
+    sys.stdout.write('\r' + b)  # \r prints a carriage return first, so `b` is printed on top of the previous line.
     time.sleep(0.5)
     if a == 4:
         a = 0
     if x == 12:
         print('\033[1;32;40m Done!')
+
+# -------------------------------------------------------------------
 
 # Weather
 # Developer: Ranen Allishaw
@@ -40,9 +47,6 @@ random.choice function to determine what the weather is
 picking from a list - using If, Elif, Else statements to
 check the condition of the road and print specific message.
 """
-
-# Import any libraries bellow
-import random
 
 """
 This function with choose a random weather condition and have that 
@@ -62,22 +66,28 @@ weatherAlert = weather()
 
 def vehicleResponseSystem():
     if weatherAlert == '\033[1;36;40mIcy':
-        print('\n\033[3;37;40mVRS has changed your alarm to 30 minutes earlier based on the NWS forcast of ' + weatherAlert + '.')
+        print(
+            '\n\033[3;37;40mVRS has changed your alarm to 30 minutes earlier based on the NWS forcast of ' + weatherAlert + '.')
         print('\033[3;37;40mVRS will only allow your car to go 30MPH')
     elif weatherAlert == 'Snowy':
-        print('\n\033[3;37;40mVRS has changed your alarm to 15 minutes earlier based on the NWS forcast of ' + weatherAlert + '.')
+        print(
+            '\n\033[3;37;40mVRS has changed your alarm to 15 minutes earlier based on the NWS forcast of ' + weatherAlert + '.')
         print('\033[3;37;40mVRS will only allow your car to go 45MPH')
     elif weatherAlert == '\033[1;34;40mRainy':
-        print('\n\033[3;37;40mVRS has changed your alarm to 12.5 minutes earlier based on the NWS forcast of ' + weatherAlert + '.')
+        print(
+            '\n\033[3;37;40mVRS has changed your alarm to 12.5 minutes earlier based on the NWS forcast of ' + weatherAlert + '.')
         print('\033[3;37;40mVRS will only allow your car to go 50MPH')
     elif weatherAlert == '\033[1;37;40mWindy':
-        print('\n\033[3;37;40mVRS has changed your alarm to 5 minutes earlier based on the NWS forcast of ' + weatherAlert + '.')
+        print(
+            '\n\033[3;37;40mVRS has changed your alarm to 5 minutes earlier based on the NWS forcast of ' + weatherAlert + '.')
         print('\033[3;37;40mVRS will only allow your car to go 60MPH')
     elif weatherAlert == '\033[1;37;40mFoggy':
-        print('\n\033[3;37;40mVRS has changed your alarm to 10 minutes earlier based on the NWS forcast of ' + weatherAlert + '.')
+        print(
+            '\n\033[3;37;40mVRS has changed your alarm to 10 minutes earlier based on the NWS forcast of ' + weatherAlert + '.')
         print('\033[3;37;40mVRS will only allow your car to go 55MPH')
     elif weatherAlert == '\033[1;33;48mThunder & Lightning':
-        print('\n\033[3;37;40mVRS has changed your alarm to 25 minutes earlier based on the NWS forcast of ' + weatherAlert + '.')
+        print(
+            '\n\033[3;37;40mVRS has changed your alarm to 25 minutes earlier based on the NWS forcast of ' + weatherAlert + '.')
         print('\033[3;37;40mVRS will only allow your car to go 35MPH')
     else:
         print('\n\033[3;37;40mVRS has not changed your alarm based on the NWS forcast of ' + weatherAlert + '.')
@@ -85,4 +95,3 @@ def vehicleResponseSystem():
 
 
 vehicleResponseSystem()
-
